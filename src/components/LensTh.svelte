2b9cab76d17b0eb1ff88f246eae03ff9f6b1e4af
vmarinovic <vmarinovic@zillowgroup.com>
2024-10-30 15:27:38 +0100

    Changes - improves design of sort asc desc icon

--- src/components/LensTh.svelte
+++ src/components/LensTh.svelte
@@ -21,9 +21,40 @@
 
 <th>
     {name}
-    <button on:click={() => sortBy(prop)}>S</button>
-    {#if sort === ASC}A{/if}
-    {#if sort === DESC}D{/if}
+    <button on:click={() => sortBy(prop)}>
+        S
+        {#if sort === ASC}<span class="sort-icon sort-icon__asc"></span>{/if}
+        {#if sort === DESC}<span class="sort-icon sort-icon__desc"></span>{/if}
+    </button>
 
     <input type="search" bind:value={searchFilter} on:change={handleSearch} />
 </th>
+
+<style>
+    button {
+        padding: 0.125em 0.5em;
+        color: grey;
+        position: relative;
+    }
+    .sort-icon {
+        display: inline-block;
+        position: relative;
+        top: 0;
+        right: 0;
+
+        width: 0;
+        height: 0;
+        border: 0.5em solid transparent;
+    }
+
+    .sort-icon__desc {
+        border-top-color: transparent;
+        border-bottom-color: white;
+    }
+
+    .sort-icon__asc {
+        top: 0.5em;
+        border-bottom-color: transparent;
+        border-top-color: white;
+    }
+</style>
